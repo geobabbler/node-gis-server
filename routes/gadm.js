1b18505c97ec878feec1b95b368d7ece3056b6b7
geobabbler <bill@geomusings.com>
2013-12-08 15:20:37 -0500

    Cleaned up routes file. Added TODO notes.

--- routes/gadm.js
+++ routes/gadm.js
@@ -1,20 +1,19 @@
 var pg = require('pg');
-var conString = "postgres://bdollins:moondog1@localhost:5432/leonardtown";
+var conString = "postgres://bdollins:moondog1@localhost:5432/leonardtown"; //TODO: point to RDS instance
 var client = new pg.Client(conString);
 
 exports.bbox = function(req, res) {
-	//query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
-//console.log(client);
     client.connect();
-    var query = client.query("select st_asgeojson(the_geom) as geojson from ltown_bldgs where cartodb_id = 1;");
+    //TODO: Select bounding box of GADM polygon based on ID, return as geojson.
+    var query = client.query("select st_asgeojson(the_geom) as geojson from ltown_bldgs where cartodb_id = 1;"); 
     var retval = "no data";
     query.on('row', function(result) {
-        console.log(result);
+        //console.log(result);
     
         if (!result) {
           return res.send('No data found');
         } else {
-          //retval = result.geojson;
+          //TODO: Do it this way or pure GeoJSON?
           res.send({id:req.params.id, shape: result.geojson});
         }
       }); 
@@ -23,5 +22,6 @@ exports.bbox = function(req, res) {
 };
  
 exports.polygon = function(req, res) {
+    //TODO: Flesh this out. Logic will be similar to bounding box.
     res.send({id:req.params.id, name: "polygon", description: "insert GeoJSON here"});
 };
