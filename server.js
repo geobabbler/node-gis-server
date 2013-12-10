761aa8bac7d03a3bb32be8da7469e53afdb5266d
Bill Dollins <bill@geomusings.com>
2013-12-10 15:41:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express'),
-    gadm = require('./routes/gadm');
+    geo = require('./routes/gadm');
  
 var app = express();
  
-app.get('/countries/:id/bbox', gadm.bbox);
-app.get('/countries/:id/bbox/:srid', gadm.bboxSrid);
-app.get('/countries/:id/polygon', gadm.polygon);
-app.get('/countries/:id/polygon/:srid', gadm.polygonSrid);
+app.get('/countries/:id/bbox', geo.bbox);
+app.get('/countries/:id/bbox/:srid', geo.bboxSrid);
+app.get('/countries/:id/polygon', geo.polygon);
+app.get('/countries/:id/polygon/:srid', geo.polygonSrid);
  
 app.listen(3000);
 console.log('Listening on port 80...');
