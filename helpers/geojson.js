f8a3a874a3a8fab24b496eff28a967e6504b43f3
Bill Dollins <bill@geomusings.com>
2014-02-18 07:43:50 -0500

    created helper module for GeoJSON tasks

---
+++ helpers/geojson.js
@@ -0,0 +1,24 @@
+exports.getFeatureResult = function(result, spatialcol) {
+		var props = new Object;
+		var crsobj = {
+			"type" : "name",
+			"properties" : {
+				"name" : "urn:ogc:def:crs:EPSG:6.3:4326"
+			}
+		};
+		for (var k in result) {
+			if (result.hasOwnProperty(k)) {
+				var nm = "" + k;
+				if ((nm != "geojson") && nm != spatialcol) {
+					props[nm] = result[k];
+				}
+			}
+		}
+
+		return {
+			type : "Feature",
+			crs : crsobj,
+			geometry : JSON.parse(result.geojson),
+			properties : props
+		};
+	};
